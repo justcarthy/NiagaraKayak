09362b35a7aedc9c038319cebec0501d089a701d
justin <jmccarthy99@gmail.com>
2016-12-21 18:05:18 -0500

    Got started on Preferences Activity

---
+++ NiagaraKayakApp/app/src/main/java/com/niagarakayak/niagarakayakapp/preferences/PreferencesActivity.java
@@ -0,0 +1,10 @@
+package com.niagarakayak.niagarakayakapp.preferences;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by justin on 21/12/16.
+ */
+
+public class PreferencesActivity extends AppCompatActivity {
+}
