aea57022080613a3edd42b3806d30fb3aaa2f30f
Bala <bv13pa@gmail.com>
2016-12-22 00:31:32 -0500

    Finished fetching reservations for a client

--- NiagaraKayakApp/app/src/main/java/com/niagarakayak/niagarakayakapp/service/reservation/ReservationAPIService.java
+++ NiagaraKayakApp/app/src/main/java/com/niagarakayak/niagarakayakapp/service/reservation/ReservationAPIService.java
@@ -1,37 +1,107 @@
 package com.niagarakayak.niagarakayakapp.service.reservation;
 
 import android.os.AsyncTask;
+
 import com.niagarakayak.niagarakayakapp.model.Reservation;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class ReservationAPIService implements ReservationService {
+
+    private String Email;  //email for getAllReservations
+    private Reservation reservation; //Reservation to be posted
     @Override
-    public void getAllReservations(ReservationCallback callback) {
+    public void getAllReservations(ReservationCallback callback,String Email) {
+        this.Email = Email;
         new getAllReservationsTask().execute(callback);
+    }
+
+    @Override
+    public void postReservation(PostCallback callback,Reservation reservation){
+        
+    }
+
+    /**
+     *
+     * @return Reservation for the given Email,null if unauthorized
+     */
+    private ArrayList<Reservation> fetchReservations(){
+        String url = UrlContainer.getReservationUrl();
+        url = String.format(url,ReservationService.APIKey,this.Email);
+        try {
+
+            HttpURLConnection httpConnection = (HttpURLConnection)new URL(url).openConnection();
+            httpConnection.setRequestMethod("GET");
+            httpConnection.setUseCaches(false);
+            httpConnection.connect();
+
+            int response_code = httpConnection.getResponseCode();
+            switch(response_code){
+                case HttpURLConnection.HTTP_OK:
+                    String json = getJSONStrign(httpConnection); //resources are closed
+                    return ReservationParser.getReservations(json);
+                case HttpURLConnection.HTTP_UNAUTHORIZED: //call failure
+                    return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    /**
+     * method gets the json object and closes all resources
+     * @param httpConnection connection to extract the JSON object from
+     * @return JSON object as string
+     * @throws Exception throws IOException
+     */
+    private String getJSONStrign(HttpURLConnection httpConnection) throws IOException {
+        StringBuffer buffer = new StringBuffer();
+        BufferedReader in = new BufferedReader(
+                                new InputStreamReader(httpConnection.getInputStream()));
+
+        String line = null;
+        while (  (line = in.readLine()) != null ){
+            buffer.append(line + "\r\n");
+        }
+        in.close();
+        httpConnection.disconnect();
+        return buffer.toString();
     }
 
 
     private class getAllReservationsTask extends AsyncTask<ReservationCallback, Void, ArrayList<Reservation>> {
-        private Exception exception;
         private ReservationCallback callback;
 
         @Override
         protected ArrayList<Reservation> doInBackground(ReservationCallback... params) {
             this.callback = params[0];
-
-            // Request goes here
-
-            return null;
+            return fetchReservations();
         }
 
         @Override
         protected void onPostExecute(ArrayList<Reservation> reservations) {
-            if (exception != null) {
+            if(reservations==null)
                 callback.onFailure();
-            } else {
-                callback.onSuccess(reservations);
-            }
+            else callback.onSuccess(reservations);
+        }
+    }
+
+    private class PostReservationTask extends AsyncTask<PostCallback,Void,Boolean> {
+        @Override
+        protected Boolean doInBackground(PostCallback... params) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
         }
     }
 }
--- NiagaraKayakApp/app/src/main/java/com/niagarakayak/niagarakayakapp/service/reservation/ReservationService.java
+++ NiagaraKayakApp/app/src/main/java/com/niagarakayak/niagarakayakapp/service/reservation/ReservationService.java
@@ -6,11 +6,24 @@ import java.util.ArrayList;
 
 public interface ReservationService {
 
-    void getAllReservations(ReservationCallback callback);
+    static final String APIKey ="F6FCED2D6429CEB07C4B9CDAAA1425435935EA537434A5F753FF40F78F446A53";
+
+    /**
+     *
+     * @param callback callback to use
+     * @param Email email address of client to fetch reservations for
+     */
+    void getAllReservations(ReservationCallback callback,String Email);
+    void postReservation(PostCallback callback,Reservation reservation);
 
     interface ReservationCallback {
         void onFailure();
         void onSuccess(ArrayList<Reservation> reservations);
+    }
+
+    interface PostCallback{
+        void onFailure(int responseCode,String message);
+        void onSuccess();
     }
 
 }
