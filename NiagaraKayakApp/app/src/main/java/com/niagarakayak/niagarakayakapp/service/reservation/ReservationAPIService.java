048c54cc8c32f174a18fbb0189d3e88e3adbb788
fzxt <fahadahmad41@hotmail.com>
2016-12-21 22:19:56 -0500

    Update contract for Reservations
    - Flesh out the contract, include callback
    - Define AsyncTask for getting reservations

--- NiagaraKayakApp/app/src/main/java/com/niagarakayak/niagarakayakapp/service/reservation/ReservationAPIService.java
+++ NiagaraKayakApp/app/src/main/java/com/niagarakayak/niagarakayakapp/service/reservation/ReservationAPIService.java
@@ -1,13 +1,37 @@
 package com.niagarakayak.niagarakayakapp.service.reservation;
 
+import android.os.AsyncTask;
 import com.niagarakayak.niagarakayakapp.model.Reservation;
 
 import java.util.ArrayList;
 
 public class ReservationAPIService implements ReservationService {
-    // TODO: Finish this
     @Override
-    public ArrayList<Reservation> getAllReservations() {
-        return null;
+    public void getAllReservations(ReservationCallback callback) {
+        new getAllReservationsTask().execute(callback);
+    }
+
+
+    private class getAllReservationsTask extends AsyncTask<ReservationCallback, Void, ArrayList<Reservation>> {
+        private Exception exception;
+        private ReservationCallback callback;
+
+        @Override
+        protected ArrayList<Reservation> doInBackground(ReservationCallback... params) {
+            this.callback = params[0];
+
+            // Request goes here
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Reservation> reservations) {
+            if (exception != null) {
+                callback.onFailure();
+            } else {
+                callback.onSuccess(reservations);
+            }
+        }
     }
 }
