0cabac3b7f4a677df6503586f3df28609bfa61ea
Bala <bv13pa@gmail.com>
2017-01-01 22:35:01 -0500

    Finished Adding and updating customer info

--- NiagaraKayakApp/app/src/main/java/com/niagarakayak/niagarakayakapp/service/reservation/UrlContainer.java
+++ NiagaraKayakApp/app/src/main/java/com/niagarakayak/niagarakayakapp/service/reservation/UrlContainer.java
@@ -6,22 +6,56 @@ package com.niagarakayak.niagarakayakapp.service.reservation;
 
 public class UrlContainer {
 
-    private static final String allReservationsUrl = "www.niagarakayak.com/kayakAPI/GetReservations.php/?api=%s&ALL=true";
-
     private static final String reservationUrl = "http://www.niagarakayak.com/kayakAPI/" +
             "GetReservations.php/?api=%s&customer=%s";
-    private static final String postURL = "http://www.niagarakayak.com/kayakAPI/MakeReservation.php/" +
+    private static final String makeReservationURL = "http://www.niagarakayak.com/kayakAPI/MakeReservation.php/" +
             "?api=%s&ReservationID=%s&Email=%s&Date=%s&Time=%s&Hours=%d&Single=%d&Tandem=%d" +
             "&location=%s&adults=%d&children=%d";
-    //apikey , email , date , time , hours , single , tandem;
 
-    public static String getAllReservationsUrl() {
-        return allReservationsUrl;
-    }
+    private static final String postCustomerURL = "http://www.niagarakayak.com/kayakAPI/customer.php/?type=POST";
+    private static final String PostCustomerURLparam = "api=%s&Email=%s&Name=%s&Phone=%s";
+
+    private static final String updateCustomerURL = "http://www.niagarakayak.com/kayakAPI/customer.php/?type=UPDATE";
+    private static final String updateCustomerURLparam = "api=%s&Email=%s&NameUP=%s&PhoneUP=%s";
+
+    /**
+     *
+     * @return URL to update customer record
+     */
+    public static String getUpdateCustomerURL() {return updateCustomerURL;}
+
+    /**
+     *
+     * @return parameter string to be passed for updating record
+     */
+    public static String getUpdateCustomerURLparam() {return updateCustomerURLparam;}
+
+    /**
+     *
+     * @return  URL to fetch reservations for a customer
+     */
     public static String getReservationUrl(){
         return reservationUrl;
     }
-    public static String getPostURL(){
-        return postURL;
+
+
+    /**
+     *
+     * @return URL to call script to send email to Buisness owner
+     */
+    public static String getmakeReservationURL(){
+        return makeReservationURL;
     }
+
+    /**
+     *
+     * @return parameter to be supplied to post customers to the table
+     */
+    public static String getPostCustomerURLparam() {return PostCustomerURLparam; }
+
+    /**
+     *
+     * @return url for posting customer to the table
+     */
+    public static String getPostCustomerURL() {return postCustomerURL; }
 }
