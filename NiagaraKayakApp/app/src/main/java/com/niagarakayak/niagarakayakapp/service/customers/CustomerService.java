9f7c1eb7af957dba63cd21731a6790a6906016c6
Bala <bv13pa@gmail.com>
2017-01-03 12:37:27 -0500

    Updated interface to support callbacks

--- NiagaraKayakApp/app/src/main/java/com/niagarakayak/niagarakayakapp/service/customers/CustomerService.java
+++ NiagaraKayakApp/app/src/main/java/com/niagarakayak/niagarakayakapp/service/customers/CustomerService.java
@@ -8,7 +8,7 @@ public interface CustomerService {
      * It should throw an CustomerExistsException if a customer exists.
      * It should throw a general exception for any networking related exceptions.
      */
-    void postCustomer(String name, String email, String phone) throws CustomerExistsException, Exception;
+    void postCustomer(String name, String email, String phone,CustomerCallback callback);
 
     /**
      * To update a row in the customers table.
@@ -18,20 +18,23 @@ public interface CustomerService {
      * @param newPhone      The desired phone
      * @throws Exception    If any networking issues, it throws an exception.
      */
-    void updateCustomer(String emailID, String newName, String newPhone) throws Exception;
+    void updateCustomer(String emailID, String newName, String newPhone,CustomerCallback callback);
 
     /**
      * Sends a verification email to a customers email.
      * @param email         The email of the customer.
      * @throws Exception    If any errors are found, an exception should be thrown.
      */
-    void sendVerificationEmail(String email) throws Exception;
+    void sendVerificationEmail(String email,CustomerCallback callback) throws Exception;
 
     /**
      * Verifies from the server if the code was valid or not.
+     * @param email                 Email of client whose code is being verified
      * @param verificationCode      Verification code input by the user to check.
+     * @throws InvalidValidationCode wrong or expired validation code
+     * @throws  Exception Connection or server related errors
      */
-    void verify(String verificationCode);
+    void verify(String email,String verificationCode,CustomerCallback callback) throws InvalidValidationCode,Exception;
 
     /**
      * This exception should be thrown if a customer exists
@@ -41,4 +44,26 @@ public interface CustomerService {
             super("A user with this email already exists. Please log in if you already have an account.");
         }
     }
+
+    /**
+     *  Exception thrown when customer enters wrong validation code or
+     *  when the validation code is expired
+     */
+    class InvalidValidationCode extends Exception{
+        public InvalidValidationCode() {
+            super("The validation code provided by the user is expired or wrong");
+        }
+    }
+
+    /**
+     * Interface for all callbacks related to service
+     *  Check the instance of Exception to display appropriate error message
+     *  CustomerExistsException if customer record already exists
+     *  InvalidValidationCode if customer entered wrong or expired code
+     */
+    interface CustomerCallback{
+        void onFailure(Exception ex);
+        void onSuccess();
+    }
+
 }
